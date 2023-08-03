a9a5a940470d922d5dd6815ab14dde5c1cbbcdde
Sophie-onbelet <84244403+Sophie-onbelet@users.noreply.github.com>
2023-08-03 12:21:18 -0600

    Delete Address.ts
    moved to folder

--- models/Address.ts
+++
@@ -1,9 +0,0 @@
-import { faker } from '@faker-js/faker';
-
-export class Address {
-  currentAddress: string;
-
-  constructor() {
-    this.currentAddress = faker.location.streetAddress({ useFullAddress: true });
-  }
-}
